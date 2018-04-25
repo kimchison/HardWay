a072cfc19b73ba3973080a1a300c5c38fdf8b9ff
Adam Wallander <framtidenskrig@hotmail.se>
2018-04-25 17:35:51 +0200

    ex21

--- ex21.py
+++ ex21.py
@@ -7,3 +7,22 @@ def subtract(a, b):
 
 def multiply(a, b):
 	print "MULTIPLYING %d * %d" % (a, b)
+
+def divide(a, b):
+	print "DIVIDING %d / %d" % (a, b)
+
+print "Lets do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+#Puzzle for credits :D
+print "Here's a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
